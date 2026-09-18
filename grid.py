b12aa7b07db622bc0531a8e609ae9ec3ba210660
Alex Smith <alexsmith9gt@gmail.com>
2026-09-18 16:50:38 -0400

    Creating the visualization of the grid

--- grid.py
+++ grid.py
@@ -1,5 +1,6 @@
 from robots import *
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 class Grid:
     def __init__(self, size:int):
         self.max_eval_step = 0
@@ -17,6 +18,21 @@ class Grid:
         # TODO
 
     def visualize(self):
+        # Creating the initial grid plot
+        fig, ax = plt.subplots()
+
+        # framing the grid
+        ax.set_xlim(-0.5,(self.n  +0.5))
+        ax.set_ylim(-0.5,(self.n + 0.5))
+
+        # Tick Marks and grid lines
+        ax.xaxis.set_major_locator(MultipleLocator(1))
+        ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+        ax.yaxis.set_major_locator(MultipleLocator(1))
+        ax.yaxis.set_minor_locator(MultipleLocator(0.5))
+        ax.grid(which="minor")
+        ax.set_title(f"Robot Grid Navigation - {self.n}x{self.n} - t = {self.current_step}")
+        plt.show()
         pass
 
     def step_forward(self):
@@ -30,6 +46,10 @@ class Grid:
     def step_backward(self):
         if self.current_step > 0:
             self.current_step -= 1
+    def add_robot(self, rob):
+        if type(rob) == Robot:
+            self.robots.append(rob)
+    
 
 class Cell:
     def __init__(self, x, y):
@@ -38,7 +58,8 @@ class Cell:
         #robots currently occupying the cell
         self.occupants = []
         #list of robots that have a goal in this cell
-        self.targets = []
+        self.goals = []
         #robots that are staged to move to this cell in the next timestep
         self.staged_move = []
-    
+g = Grid(5)
+g.visualize()
